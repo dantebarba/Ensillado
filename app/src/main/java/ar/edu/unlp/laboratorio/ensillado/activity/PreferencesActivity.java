f6eb600af6b5f1e0f83e620166c31e7b1102fce2
Greenhill Jorge <greenhill.j.m@gmail.com>
2017-02-10 15:01:50 -0300

    se agrega menu configuracion

---
+++ app/src/main/java/ar/edu/unlp/laboratorio/ensillado/activity/PreferencesActivity.java
@@ -0,0 +1,17 @@
+package ar.edu.unlp.laboratorio.ensillado.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class PreferencesActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferencesFragment()).commit();
+    }
+
+}
+
+
